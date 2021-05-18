a5982f86b60c8c485b53f64679e03153fa66c254
mariajantz <mkjantz@gmail.com>
2021-05-18 10:45:26 -0400

    temporary remove mux files for weird git thing

--- mux_library/mux_v1/switch_mux.m
+++
@@ -1,9 +0,0 @@
-function switch_mux(ser,arr)
-% send command string to ML MUX controller box over the active serial port
-%  ser -- active serial port
-%  arr -- MUX command bytes = [length, e-cell, 1-or-2, e-cell, 1-or-2, ...]
-
-    fwrite(ser, [encode_cobs([length(arr) arr]) 0 MLMUX.SWITCH_MUX]);
-    %ser.write(encode_cobs([length(arr) arr 0 MLMUX.SWITCH_MUX]),'uint8')
-end
-
